aa71d6728f5f494e1ceadb8f9eaa92c65b69b73f
gerrdevs <paletgerardo.devs@gmail.com>
2024-05-26 18:05:49 -0300

    init

---
+++ src/main/java/com/devsnology/taller/javarestbasic/JavarestbasicApplication.java
@@ -0,0 +1,13 @@
+package com.devsnology.taller.javarestbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavarestbasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavarestbasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsnology/taller/javarestbasic/JavarestbasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsnology.taller.javarestbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavarestbasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
